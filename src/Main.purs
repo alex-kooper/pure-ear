f330155c12c662b150915debcebfd2f16154d4aa
Alex Kuprienko <akuprienko@williamhill.us>
2023-09-19 23:09:48 -0400

    Replaced deprecated function

--- src/Main.purs
+++ src/Main.purs
@@ -1,12 +1,11 @@
 module Main where
 
 import Prelude
-
-import PureEar.Page.Home (mkHome)
 import Data.Maybe (Maybe(..))
 import Effect (Effect)
 import Effect.Exception (throw)
-import React.Basic.DOM (render)
+import PureEar.Page.Home (mkHome)
+import React.Basic.DOM.Client (createRoot, renderRoot)
 import Web.DOM.NonElementParentNode (getElementById)
 import Web.HTML (window)
 import Web.HTML.HTMLDocument (toNonElementParentNode)
@@ -16,9 +15,8 @@ main :: Effect Unit
 main = do
   root <- getElementById "root" =<< (map toNonElementParentNode $ document =<< window)
   case root of
-    Nothing ->
-      throw "Root element not found."
-    Just r  -> do
+    Nothing -> throw "Root element not found."
+    Just r -> do
       home <- mkHome
-      render (home unit) r
-
+      reactRoot <- createRoot r
+      renderRoot reactRoot (home unit)
